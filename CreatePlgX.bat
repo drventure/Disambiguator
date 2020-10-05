c6bd35a6ed4eed56a9c724f5c77e2d996624b52b
Darin Higgins <dhiggins@worksoft.com>
2020-10-05 07:46:13 -0500

    Trying to get the plgx to properly compile again

--- CreatePlgX.bat
+++ CreatePlgX.bat
@@ -1,20 +1,28 @@
-echo !!!!!! Packaging Release !!!!!!
-cd %~dp0
+echo on
+echo !!!!!! Building Plgx !!!!!!
+cd /D %~dp0
 
 echo Deleting existing PlgX folder
-rmdir /s /q PlgX
+if exist Plgx rmdir /s /q PlgX
 
 echo Creating PlgX folder
-mkdir PlgX
+if not exist Plgx\nul mkdir PlgX
+
+echo Make sure the Releases folder exists
+if not exist "%~dp0Releases\NUL" mkdir "%~dp0Releases"
+
+echo Make sure the Build Output folder exists
+if not exist "%~dp0Releases\Build Outputs\NUL" mkdir "%~dp0Releases\Build Outputs"
 
 echo Copying files
 xcopy "Disambiguator" PlgX /s /e /exclude:PlgXExclude.txt
 
 echo Compiling PlgX
-"./Disambiguator/KeePass.exe" /plgx-create "%~dp0PlgX" --plgx-prereq-os:Windows
+".\Disambiguator\KeePass.exe" /plgx-create "%~dp0PlgX" --plgx-prereq-os:Windows
 
 echo Releasing PlgX
-move /y PlgX.plgx "Releases\Build Outputs\Disambiguator.plgx"
+move /y PlgX.plgx ".\Releases\Build Outputs\Disambiguator.plgx"
+copy "%~dp0Readme.txt" ".\Releases\Build Outputs\"
 
 echo Cleaning up
 rmdir /s /q PlgX
--- PackageRelease.bat
+++ PackageRelease.bat
@@ -1,28 +1,22 @@
+echo on
 echo !!!!!! Packaging Release !!!!!!
+
+cd /D %~dp0
+
 echo Setting up
+echo Current Dir is %CD%
 set version=%1
-set output=%~dp0Releases\v%version%\
-set zipfile="%output%Disambiguator-v%version%.zip"
-set buildoutputs="%~dp0Releases\Build Outputs"
+set buildoutputs=%~dp0Releases\Build Outputs
+set output=%~dp0Releases\v%version%
+set zipfile=%output%\Disambiguator-v%version%.zip
 
-echo Make sure the Releases folder exists
-mkdir "%~dp0Releases"
+echo Remove Output Version folder %output% to start fresh
+if exist "%output%\NUL" rmdir /s /q "%output%"
 
-echo Remove Output Version folder to start fresh
-rmdir /s /q "%output%"
-
-echo Remove the Build output folder as well
-rmdir "%buildoutputs%"
-
-echo Create the build outputs folder
-mkdir "%buildoutputs%"
-
-copy "%~dp0Readme.txt" %buildoutputs%
-
-pushd "%buildoutputs%""
-"%ProgramFiles%\7-Zip\7z.exe" a -tzip -mx9 -bd %zipfile% *
+pushd "%buildoutputs%"
+"%ProgramFiles%\7-Zip\7z.exe" a -tzip -mx9 -bd "%zipfile%" *
 popd
-copy "%~dp0Readme.txt" "%output%"
+copy "%~dp0Readme.txt" "%output%\"
 
 set version=
 set output=
