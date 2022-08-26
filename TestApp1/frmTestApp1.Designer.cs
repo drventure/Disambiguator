061dc6078128cf477542ecf340fc7c79063dd91b
Darin Higgins <dhiggins@worksoft.com>
2022-08-26 09:44:58 -0500

    Added a little more debugging code to the test app.

--- TestApp1/frmTestApp1.Designer.cs
+++ TestApp1/frmTestApp1.Designer.cs
@@ -31,12 +31,18 @@
             this.picMain = new System.Windows.Forms.PictureBox();
             this.lblPassword = new System.Windows.Forms.Label();
             this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
             this.SuspendLayout();
             // 
             // picMain
             // 
-            this.picMain.Location = new System.Drawing.Point(12, 24);
+            this.picMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picMain.Location = new System.Drawing.Point(12, 22);
             this.picMain.Name = "picMain";
             this.picMain.Size = new System.Drawing.Size(522, 272);
             this.picMain.TabIndex = 0;
@@ -58,15 +64,49 @@
             this.tbxPassword.Size = new System.Drawing.Size(200, 31);
             this.tbxPassword.TabIndex = 2;
             // 
-            // Form1
+            // tbxOutput
+            // 
+            this.tbxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxOutput.Location = new System.Drawing.Point(25, 106);
+            this.tbxOutput.Multiline = true;
+            this.tbxOutput.Name = "tbxOutput";
+            this.tbxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbxOutput.Size = new System.Drawing.Size(493, 177);
+            this.tbxOutput.TabIndex = 3;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(406, 68);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(112, 32);
+            this.btnGo.TabIndex = 4;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(231, 75);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(66, 25);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "0 of 0";
+            // 
+            // frmTestApp1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(587, 328);
+            this.ClientSize = new System.Drawing.Size(546, 304);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbxOutput);
             this.Controls.Add(this.tbxPassword);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.picMain);
-            this.Name = "Form1";
+            this.Name = "frmTestApp1";
             this.Text = "Enter Password";
             ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
             this.ResumeLayout(false);
@@ -79,6 +119,9 @@
         private System.Windows.Forms.PictureBox picMain;
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.TextBox tbxOutput;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- TestApp1/frmTestApp1.cs
+++ TestApp1/frmTestApp1.cs
@@ -6,7 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Automation;
 using System.Windows.Forms;
+
 
 namespace TestApp1
 {
@@ -15,6 +17,66 @@ namespace TestApp1
         public frmTestApp1()
         {
             InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            var root = AutomationElement.RootElement;
+
+            Recurse(root);
+        }
+
+        private void Recurse(AutomationElement element)
+        {
+            int ec = 0;
+            var elems = element.FindAll(TreeScope.Children, Condition.TrueCondition).Cast<AutomationElement>();
+
+            var e1 = elems.Where(e => Prop(e, "Name") == "Certify-Client Repos").FirstOrDefault();
+
+            elems = e1.FindAll(TreeScope.Children, Condition.TrueCondition).Cast<AutomationElement>();
+
+            e1 = elems.Where(e => Prop(e, "Name") == "Windows Security").FirstOrDefault();
+
+            elems = e1.FindAll(TreeScope.Subtree, Condition.TrueCondition).Cast<AutomationElement>();
+
+            var tc = elems.Count();
+            foreach (AutomationElement e in elems)
+            {
+                ec++;
+                try
+                {
+                    var props = e.GetSupportedProperties();
+                    tbxOutput.Text += "\r\n";
+                    int pc = 0;
+                    foreach (var p in props)
+                    {
+                        pc++;
+                        tbxOutput.Text += $"   {p.ProgrammaticName}-{e.GetCurrentPropertyValue(p)}\r\n";
+                        lblStatus.Text = $"{pc} prop in {ec} of {tc}";
+                        lblStatus.Refresh();
+                    }
+                }
+                catch 
+                { }
+            }
+        }
+
+
+        private string Prop(AutomationElement e, string propName)
+        {
+            try
+            {
+                var props = e.GetSupportedProperties();
+                foreach (var p in props)
+                {
+                    if (p.ProgrammaticName.Contains("." + propName + "Property"))
+                    {
+                        return $"{e.GetCurrentPropertyValue(p)}";
+                    }
+                }
+            }
+            catch { }
+            return "$$$UNKNOWN$$$";
         }
     }
 }
